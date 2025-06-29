b0c12fba2df7220c96da57b566b2d4809f4257f6
devFazal <140000792+devFazal@users.noreply.github.com>
2025-06-30 05:00:42 +0530

    Add files via upload

---
+++ src/utils/constants.jsx
@@ -0,0 +1,17 @@
+// src/utils/constants.js
+export const MONTHS = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+export const YEARS = [2025, 2024, 2023, 2022, 2021];
